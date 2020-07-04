26da9befdd279da50e4d862d763d004443d026a8
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2020-07-04 09:47:30 +0800

    Верстка

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('.hero').slick({
+		dots: true,
+		arrows: false, 
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
+
+
+
+
+
+
+
+
+
+
+});
